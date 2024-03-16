c32f7e90c09b3412d97d764eb0e5732feef4f32e
gosanman <60295462+gosanman@users.noreply.github.com>
2024-03-16 15:12:17 +0100

    Update

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "d1768a6"
+#define MAIN_Version "b14cf8a"
 #define KNX_Version "9d7c8ac"
 #define MODULE_Common_Version "1.1.1+84a1e7b"
 #define MODULE_Common_ETS 17
