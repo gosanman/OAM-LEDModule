9f335931fd86302212a0a1ef0cdacd25bce57be2
gosanman <60295462+gosanman@users.noreply.github.com>
2024-11-24 11:34:00 +0100

    Update

--- include/versions.h
+++ include/versions.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MAIN_Version "93e518e"
+#define MAIN_Version "747ebca"
 #define KNX_Version "2.1.1+f606f6d"
-#define MODULE_Common_Version "1.2.0+8410b2a"
+#define MODULE_Common_Version "1.2.0+aa0d7ab"
 #define MODULE_Common_ETS 18
-#define MODULE_FileTransferModule_Version "0.0.4+db4a240"
+#define MODULE_FileTransferModule_Version "0.0.4+f13e94b"
 #define MODULE_FileTransferModule_ETS 0
