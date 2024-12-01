e359421d78ff28fccbe607576253ea40d74519b3
gosanman <60295462+gosanman@users.noreply.github.com>
2024-12-01 15:49:38 +0100

    Update

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "fa31b20"
+#define MAIN_Version "6231e4e"
 #define KNX_Version "2.1.1+f606f6d"
 #define MODULE_Common_Version "1.2.0+b93a245"
 #define MODULE_Common_ETS 18
