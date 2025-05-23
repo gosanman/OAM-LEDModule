6a292428f4067c84cc4241ff3c958d2fd2b04b90
gosanman <60295462+gosanman@users.noreply.github.com>
2025-05-23 14:10:12 +0200

    update Build-knxprod.ps1 to use the new version of Kaenx-Creator

--- scripts/Build-knxprod.ps1
+++ scripts/Build-knxprod.ps1
@@ -7,41 +7,44 @@ if (-Not (Test-Path -Path platformio.ini)) {
     exit 1
 }
 $projectDir = Get-Location
-
 $files = Get-ChildItem -Path $projectDir/kaenx/*.ae-manu
-$filePath = Join-Path -Path $projectDir -ChildPath "kaenx" | Join-Path -ChildPath $files[0].Name
-~/bin/Kaenx.Creator.Console publish $filePath
+
+if ($files.Count -eq 0) {
+    Write-Host "Keine .ae-manu Dateien gefunden im Ordner kaenx."
+    exit 1
+}
+Write-Host "Verfuegbare .ae-manu Dateien:"
+for ($i = 0; $i -lt $files.Count; $i++) {
+    Write-Host "$($i+1): $($files[$i].Name)"
+}
+
+$selection = Read-Host "Bitte waehlen eine Datei zur Verarbeitung aus (1-$($files.Count))"
+
+if ($selection -match "^[1-9]\d*$") {
+    $selectedIndex = [int]$selection - 1
+    if ($selectedIndex -ge 0 -and $selectedIndex -lt $files.Count) {
+        $selectedFile = $files[$selectedIndex]
+        $filePath = Join-Path -Path $projectDir -ChildPath "kaenx" | Join-Path -ChildPath $selectedFile.Name
+        & "$HOME/bin/Kaenx.Creator.Console" publish $filePath
+    } else {
+        Write-Host "Ungueltige Auswahl."
+        exit 1
+    }
+} else {
+    Write-Host "Ungueltige Eingabe."
+    exit 1
+}
 
 Write-Host ""
 $knxprod = Get-ChildItem -Path $projectDir/kaenx/*.knxprod
 $knxprodname = $knxprod[0].Name
 Write-Host "Die KNX-Produktdatei $knxprodname wurde erfolgreich erstellt."
-
-# KO-Definitionen in der generierten Datei anpassen
-# BASE_Ko und KoBASE_ Definitionen anpassen
 $generatedFile = "$projectDir/kaenx/knxprod.h"
-Copy-Item -Path $generatedFile -Destination $projectDir/kaenx/knxprod_org.h_ -Force
-Write-Host "Sicherungskopie von knxprod.h erstellt: knxprod_org.h_"
-$content = Get-Content $generatedFile -Raw
-
-# Regex pattern für BASE_Ko und KoBASE_ Definitionen
-$pattern = '(?m)^(//!<[^\n]+\n)#define\s+(BASE_Ko\w+)\s+(\d+)\s*\n#define\s+(Ko\w+)\s+knx\.getGroupObject\([^)]+\)'
-$replacement = {
-    param($match)
-    $comment = $match.Groups[1].Value
-    $baseName = $match.Groups[2].Value
-    $number = $match.Groups[3].Value
-    $koName = $match.Groups[4].Value
-    
-    "${comment}#define $baseName $number + BASE_Share_KoOffset`n#define $koName knx.getGroupObject($baseName)"
-}
-$newContent = [regex]::Replace($content, $pattern, $replacement)
-$newContent | Set-Content $generatedFile -NoNewline
-
-# Verschiebe die angepasste Datei nach include
+# Verschiebe die knxprod.h Datei nach include
 $targetPath = Join-Path -Path $projectDir -ChildPath "include\knxprod.h"
 if (Test-Path $targetPath) {
     Remove-Item $targetPath -Force
 }
 Move-Item $generatedFile $targetPath
 Write-Host "Die knxprod.h wurde erstellt und nach include verschoben."
+Write-Host ""
